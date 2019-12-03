ba60784a9da259d37132db0cb5c5e66bb22a6567
Eric Smith <esmith@bastiansolutions.com>
2019-12-03 15:56:06 -0600

    print two wires

--- 03/crossed_wires.py
+++ 03/crossed_wires.py
@@ -13,7 +13,7 @@ def manhattan_distance(x,y):
 class CrossedWires:
     def __init__(self, x, y, org_x, org_y):
         self.graph = pd.DataFrame([[0 for _ in range(y)] for _ in range(x)])
-        self.origin = (org_x, org_y)
+        self.origin = (org_y, org_x)
         self.graph.iloc[org_y][org_x] = -1
         self.closest_intersection = None
 
@@ -22,24 +22,29 @@ class CrossedWires:
             csv_file = csv.reader(f)
 
             for i, wire in enumerate(csv_file):
-                print(i)
+                cur_x = self.origin[1]
+                cur_y = self.origin[0]
+                
                 for loc in wire:
+                    
                     direction = loc[0]
                     length = int(loc[1:])
                     
-                    cur_x = self.origin[1]
-                    cur_y = self.origin[0]
+                    print(cur_x, cur_y)
 
                     if direction == 'U':
-                        self.graph[self.graph.iloc[cur_y:cur_y+length][cur_x][i]] = 1
+                        self.graph.iloc[cur_y-length:cur_y, cur_x] += 1
+                        cur_y -= length
                     elif direction == 'D':
-                        self.graph[self.graph.iloc[cur_y-length:cur_y][cur_x][i]] = 1
+                        self.graph.iloc[cur_y:cur_y+length+1, cur_x] += 1
+                        cur_y += length+1
                     elif direction == 'R':
-                        self.graph[self.graph.iloc[cur_y][cur_x:cur_x+length][i]] = 1
+                        self.graph.iloc[cur_y, cur_x+1:cur_x+length+1] += 1
+                        cur_x += length
                     elif direction == 'L':
-                        self.graph[self.graph.iloc[cur_y][cur_x-length:cur_x][i]] = 1
-
-                    # self.print_grid()
+                        self.graph.iloc[cur_y, cur_x-length:cur_x] += 1
+                        cur_x -= length+1
+                    # print(self.graph)
 
     def find_intersections(self):
         if 1 > 0:
@@ -49,10 +54,15 @@ class CrossedWires:
             pass
 
 if __name__ == "__main__":
-    my_graph = CrossedWires(10,11, 0, 9)
-    my_graph.graph.iloc[0:5, 0] += 1
-    # for col in my_graph.graph[0:5]:
-    #     my_graph.graph.loc[0, col] = [5,5]
+    my_graph = CrossedWires(10,11, 1, 8)
+    # my_graph.graph.iloc[0:5, 0] += 1
 
     print(my_graph.graph)
-    # my_graph.lay_wires('03/input2.csv')
+    print("---------------")
+    my_graph.lay_wires('03/input2.csv')
+    print(my_graph.graph)
+
+"""
+R8,U5,L5,D3
+U7,R6,D4,L4
+"""
